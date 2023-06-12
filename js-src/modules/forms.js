58d7a5ec3c2fd3f45663bbcd05d699d114fedd8f
Victoria <121200782+odinaeva-vika@users.noreply.github.com>
2023-06-12 17:19:52 +0300

    init
    доб обработчик

--- js-src/modules/forms.js
+++ js-src/modules/forms.js
@@ -3,27 +3,27 @@ const taskForm = document.getElementById("taskForm"),
       taskBox = document.getElementById("taskBox");
 
 const getInputValue = () => taskInput.value;
-const arrlists = [];
+const taskLists = [];
 
 const getTaskForm = (e) => {
-   e.preventDefault();
+  e.preventDefault();
 
   const inputValue = getInputValue();
   if (inputValue.trim().length === 0) {
     return;
   }
   createList();
-  renderNewListFromUser();
+  renderTaskList();
   clearInput();
 };
 
 function List(title) {
   this.list = title;
   this.completed = false;
-};
+}
 
 const createList = () => {
-  arrlists.push(new List(getInputValue()));
+  taskLists.push(new List(getInputValue()));
 };
 
 const clearInput = () => {
@@ -34,9 +34,9 @@ const clearInput = () => {
 const createListItemHTML = (item, index) => `
 <div class="box ${item.completed ? 'box--action' : ''}" data-index="${index}">
   <label class="box__label">
-                <input class="box__input" type="checkbox" value="" ${item.completed ? 'checked' : ''}>
-                <span data-action="done"></span>
-              </label>
+    <input class="box__input" type="checkbox" value="" ${item.completed ? 'checked' : ''}>
+    <span data-action="done"></span>
+  </label>
   <h2 class="box__title">${item.list}</h2>
   <button data-action="delete" class="box__button">
     <img src="./img/close.svg" alt="Удалить фильм из списка">
@@ -44,32 +44,40 @@ const createListItemHTML = (item, index) => `
 </div>
 `;
 
-const renderNewListFromUser = () => {
-  taskBox.innerHTML = arrlists
+const renderTaskList = () => {
+  taskBox.innerHTML = taskLists
     .map((item, index) => createListItemHTML(item, index))
     .join('');
 };
 
-const deleteTaskBox = (e) => {
+const deleteTask = (e) => {
   if (e.target.dataset.action === "delete") {
     const parentNode = e.target.closest(".box");
     const index = parseInt(parentNode.dataset.index, 10);
     parentNode.remove();
-    arrlists.splice(index, 1);
+    taskLists.splice(index, 1);
   }
-  return
+  return;
 };
 
-const doneTaskBox = (e) => {
+const toggleTaskCompletion = (e) => {
   if (e.target.dataset.action === "done") {
     const parentNode = e.target.closest(".box");
     parentNode.classList.toggle("box--action");
     const index = parseInt(parentNode.dataset.index, 10);
-    arrlists[index].completed = parentNode.classList.contains("box--action");
+    taskLists[index].completed = parentNode.classList.contains("box--action");
   }
-  return
+  return;
+};
+
+const handleTaskAction = (e) => {
+  const action = e.target.dataset.action;
+  if (action === "delete") {
+    deleteTask(e);
+  } else if (action === "done") {
+    toggleTaskCompletion(e);
+  }
 };
 
 taskForm.addEventListener("submit", getTaskForm);
-taskBox.addEventListener("click", deleteTaskBox);
-taskBox.addEventListener("click", doneTaskBox);
+taskBox.addEventListener("click", handleTaskAction);
